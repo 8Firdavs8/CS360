37dbf910490bd950378c54c7d49a3d887ff9802b
8Firdavs8 <atabfi01@luther.edu>
2021-09-17 16:26:02 -0500

    exercise1

--- src/exercises/hashing/hashing.py
+++ src/exercises/hashing/hashing.py
@@ -4,25 +4,52 @@
 
 def hash_remainder(key: int, size: int) -> int:
     """Find hash using remainder"""
-    raise NotImplementedError
+    return key%size 
 
+# print(hash_remainder(13,3))
 
 def hash_mid_sqr(key: int, size: int) -> int:
     """Find hash using mid-square method"""
-    raise NotImplementedError
+    return int(str(key * key) \
+        [len(str(key * key)) // 2-1:len(str(key * key)) // 2+1]) % size
 
 
 def hash_folding(key: int, size: int) -> int:
     """Find hash using folding method"""
-    raise NotImplementedError
+    # if (len(key))%2 == 0:
+    #     firstHalf = key[:len(key) // 2]
+    #     secondHalf = key[firstHalf: ]
+    #     for x in firstHalf:
+    #         first = x%size
+    #     for y in secondHalf:
+    #         second = y%size
+    #     return (first + second)/size
+    return NotImplemented
+    
 
+
+        
+
+    
+
+
+    
+print(hash_folding(123,3))
 
 def hash_str(key: str, size: int) -> int:
     """Find string hash using simple sum-of-values method"""
-    raise NotImplementedError
+    num = 0
+    for i in range(len(key)):
+        num += ord(key[i])
+    return (num % size)
 
 
 def hash_str_weighted(key: str, size: int) -> int:
     """Find string hash using character positions as weights"""
-    raise NotImplementedError
+    num = 0
+    for i in range(len(key)):
+        num += ord(key[i])*i
+    return (num % size)
+
+
 
