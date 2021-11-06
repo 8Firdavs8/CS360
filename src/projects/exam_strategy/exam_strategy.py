09d9c5d379d5de26a651c8cb7ecab20b3f992e98
8Firdavs8 <atabfi01@luther.edu>
2021-11-06 14:52:57 -0500

    prject

--- src/projects/exam_strategy/exam_strategy.py
+++ src/projects/exam_strategy/exam_strategy.py
@@ -20,7 +20,39 @@ def knapsack(capacity: int, items: list[Item]) -> list[int]:
     :return: a list of chosen indices
     """
     # TODO: Implement this function
-    ...
+    # Get all the weights and values
+    value, weight, theMatrix = [], [], []
+    for i in items: value.append(i[0])
+    for i in items: weight.append(i[1])
+    row = weight[0]
+    # Create all 0s following the first law
+    theMatrix = [[0 for i in range(capacity+1)] for i in range(row+1)]
+    # Add second and third law
+    value.pop(0)
+    weight.pop(0)
+    for i in range(row + 1): 
+        for j in range(capacity + 1): 
+            if i == 0 or j == 0:  theMatrix[i][j] = 0
+            # Third law
+            elif weight[i-1] <= j: theMatrix[i][j] = max(value[i-1] + theMatrix[i-1][j-weight[i-1]],  theMatrix[i-1][j]) 
+            # Second law
+            else: theMatrix[i][j] = theMatrix[i-1][j] 
+    
+    
+    # Getting  the last item of the matrix  
+    c, w, res = theMatrix[row][capacity], capacity, list()
+    cond = True
+    for i in range(row, 0, -1): 
+        if theMatrix[row][capacity] <= 0: cond is False
+        elif theMatrix[row][capacity] == theMatrix[i - 1][w] and cond is True: continue
+        else: 
+            res.append([i-1])
+            res.append(weight[i - 1]) 
+            theMatrix[row][capacity] -= value[i - 1] 
+            w -= weight[i - 1] 
+    final=[j for i in res[::2] for j in i]
+    final.reverse()
+    return [final, c]
 
 
 def pick_questions_to_answer(filename: str) -> tuple[list[int], int]:
@@ -31,7 +63,14 @@ def pick_questions_to_answer(filename: str) -> tuple[list[int], int]:
     :return: the list of chosen indices and total point value of all selected questions
     """
     # TODO: Implement this function
-    ...
+    myList = []
+    with open(filename) as f:
+        for aline in f:
+            aline = aline.split()
+            if aline:          
+                aline = [int(float(i)) for i in aline]
+                myList.append(aline)
+    return (knapsack(myList[0][0], myList))
 
 
 def main():
