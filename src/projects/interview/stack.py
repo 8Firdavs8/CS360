7f1c19df2e52873b971265354c9e051f2e8089d3
8Firdavs8 <atabfi01@luther.edu>
2021-12-02 20:53:43 -0600

    project

--- src/projects/interview/stack.py
+++ src/projects/interview/stack.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/env python3
 """
-`stack` implementation
+stack implementation
 
 @authors: Roman Yasinovskyy
 @version: 2021.11
@@ -29,50 +30,17 @@ class Stack:
         """Initialize a stack using heapq"""
         # NOTE: Do not modify this method
         self.items = []
-    
-    def siftdown(heap, startpos, pos):
-        newitem = heap[pos]
-        # Follow the path to the root, moving parents down until finding a place
-        # newitem fits.
-        while pos > startpos:
-            parentpos = (pos - 1) >> 1
-            parent = heap[parentpos]
-            if newitem < parent:
-                heap[pos] = parent
-                pos = parentpos
-                continue
-            break
-        heap[pos] = newitem
-
-    def siftup(heap, pos):
-        endpos = len(heap)
-        startpos = pos
-        newitem = heap[pos]
-       
-        childpos = 2*pos + 1    
-        while childpos < endpos:
-            
-            rightpos = childpos + 1
-            if rightpos < endpos and not heap[childpos] < heap[rightpos]:
-                childpos = rightpos
-           
-            heap[pos] = heap[childpos]
-            pos = childpos
-            childpos = 2*pos + 1
-        
-        heap[pos] = newitem
-        heap.siftdown( startpos, pos)
-
+   
+   
     def push(self, item: Any) -> None:
+        count = 0
         """
         Add a new item to stack
 
         :param item: a new item to push onto the stack
         """
-        # TODO: Implement this method
-        self.items.append(item)
-        self.siftdown( 0, len(self.items)-1)
-
+        heapq.heappush(self.items,(count,item))
+        count = count + 1
 
     def pop(self) -> Any:
         """
@@ -81,18 +49,7 @@ class Stack:
         :return: the top element of the stack
         :raise StackError is the stack is empty
         """
-        # TODO: Implement this method
-        self.items.pop()
-        """Pop the smallest item off the heap, maintaining the heap invariant."""
-        heap = self.items
-        lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
-        if heap:
-            returnitem = heap[0]
-            heap[0] = lastelt
-            self.siftup( 0)
-            return returnitem
-        return lastelt
-
+        return heapq.heappop(self.items)
 
     def peek(self) -> Any:
         """
@@ -101,8 +58,10 @@ class Stack:
         :return: the top element of the stack
         :raise StackError is the stack is empty
         """
-        # TODO: Implement this method
-        return self.items[0]
+        
+        
+        element = self.items[0]
+        return element
 
 
     def __bool__(self) -> bool:
@@ -111,12 +70,10 @@ class Stack:
 
         :return: False if the stack is empty, True otherwise
         """
-        # TODO: Implement this method
         value = True
         if len(self.items)==0:
             value = False
         return value
-
 
     def __len__(self) -> int:
         """
@@ -124,6 +81,4 @@ class Stack:
 
         :return: number of items in the stack (0 if the stack is empty)
         """
-        # TODO: Implement this method
-        return len(self.items)==0
-
+        return len(self.items)==1
